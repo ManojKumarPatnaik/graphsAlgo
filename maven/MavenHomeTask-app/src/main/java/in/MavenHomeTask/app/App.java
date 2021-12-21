8b9e9c7464d9daf45d014f8f240ac4a94f6d5ace
Manojkumar Uriti <manojkumar_uriti@epam.com>
2021-12-21 20:37:19 +0530

    Module-2 Maven Home Task done!

---
+++ maven/MavenHomeTask-app/src/main/java/in/MavenHomeTask/app/App.java
@@ -0,0 +1,13 @@
+package in.MavenHomeTask.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Manoj_Maven_Home_Task" );
+    }
+}
